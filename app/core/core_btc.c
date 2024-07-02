e808bf52223ad9472f7f11b4e51083b48f4f56db
Michele Balistreri <michele@bitgamma.com>
2024-07-02 10:28:17 +0200

    add handler stub

--- app/core/core_btc.c
+++ app/core/core_btc.c
@@ -1,8 +1,16 @@
 #include "core.h"
 #include "bitcoin/psbt.h"
 
+void core_btc_tx_elem_handler(psbt_elem_t* rec) {
+  if (rec->type == PSBT_ELEM_RECORD) {
+
+  } else if (rec->type == PSBT_ELEM_TXELEM) {
+
+  }
+}
+
 void core_btc_psbt_run(struct zcbor_string* qr_request) {
   psbt_t psbt;
   psbt_init(&psbt, (uint8_t*) qr_request->value, qr_request->len);
-  psbt_read(qr_request->value, qr_request->len, &psbt, NULL, NULL);
+  psbt_read(qr_request->value, qr_request->len, &psbt, core_btc_tx_elem_handler, NULL);
 }
